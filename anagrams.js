1c45907b583ed1e014adc6319b904e94f2853001
Ray Yeung <raymond.zj.yeung@gmail.com>
2018-05-14 23:47:59 -0700

    added anagram

---
+++ anagrams.js
@@ -0,0 +1,14 @@
+function anagrams(prefix, str) {
+  if(str.length <= 1) {
+    console.log(`The anagram is ${prefix}${str}`);
+  } else {
+    for(let i = 0; i < str.length; i++) {
+      let current = str.substring(i, i + 1);
+      let previous = str.substring(0, i);
+      let after = str.substring(i + 1);
+      anagrams(prefix + current, previous + after);
+    }
+  }
+};
+
+console.log(anagrams('', 'east'));
