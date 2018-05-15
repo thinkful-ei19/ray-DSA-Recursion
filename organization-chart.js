9a98985e9094c151956e4477121919e57323d43c
Ray Yeung <raymond.zj.yeung@gmail.com>
2018-05-15 00:09:21 -0700

    working on organization chart

---
+++ organization-chart.js
@@ -0,0 +1,87 @@
+'use strict';
+
+// Zuckerberg
+//     Schroepfer
+//         Bosworth
+//             Steve
+//             Kyle
+//             Andra
+//         Zhao
+//             Richie
+//             Sofia
+//             Jen
+//     Schrage
+//         VanDyck
+//             Sabrina
+//             Michelle
+//             Josh
+//         Swain
+//             Blanch
+//             Tom
+//             Joe
+//     Sandberg
+//         Goler
+//             Eddie
+//             Julie
+//             Annie
+//        Hernandez
+//             Rowi
+//             Inga
+//             Morgan
+//        Moissinac
+//             Amy
+//             Chuck
+//             Vinni
+//        Kelley
+//             Eric
+//             Ana
+//             Wes
+
+const orgChart = [
+  {id: 'Zuckerberg', boss: null},
+  {id: 'Schroepfer', boss: 'Zuckerberg'},
+  {id: 'Schrage', boss: 'Zuckerberg'},
+  {id: 'Sandberg', boss: 'Zuckerberg'},
+  {id: 'Bosworth', boss:'Schroepfer' },
+  {id: 'Zhao', boss:'Schroepfer' },
+  {id: 'Steve', boss:'Bosworth' },
+  {id: 'Kyle', boss:'Bosworth' },
+  {id: 'Andra', boss:'Bosworth' },
+  {id: 'Richie', boss: 'Zhao'},
+  {id: 'Sofia', boss: 'Zhao'},
+  {id: 'Jen', boss: 'Zhao'},
+  {id: 'VanDyck', boss:'Schrage' },
+  {id: 'Swain', boss:'Schrage' },
+  {id: 'Sabrina', boss:'VanDyck' },
+  {id: 'Michelle', boss:'VanDyck' },
+  {id: 'Josh', boss:'VanDyck' },
+  {id: 'Blanch', boss:'Swain' },
+  {id: 'Tom', boss:'Swain' },
+  {id: 'Joe', boss:'Swain' },
+  {id: 'Goler', boss:'Sandberg' },
+  {id: 'Hernandez', boss:'Sandberg' },
+  {id: 'Moissinac', boss:'Sandberg' },
+  {id: 'Kelley', boss:'Sandberg' },
+  {id: 'Eddie', boss:'Goler' },
+  {id: 'Julie', boss:'Goler' },
+  {id: 'Annie', boss:'Goler' },
+  {id: 'Rowi', boss:'Hernandez'},
+  {id: 'Inga', boss:'Hernandez'},
+  {id: 'Morgan', boss:'Hernandez'},
+  {id: 'Amy', boss:'Moissinac'},
+  {id: 'Chuck', boss:'Moissinac'},
+  {id: 'Vinni', boss:'Moissinac'},
+  {id: 'Eric', boss:'Kelley'},
+  {id: 'Ana', boss:'Kelley'},
+  {id: 'Wes', boss:'Kelley'}
+];
+
+
+function organization(chart, boss) {
+  let node = {};
+  chart.filter(item => item.boss === boss)
+    .forEach(item => node[item.id] = organization(chart, item.id));
+  return node;
+};
+
+console.log(organization(orgChart, 0));
